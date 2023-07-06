2ee9a1bf6dadcad56be9a1ec1b2e962d96aa0987
Diego <diegopaulino_3@hotmail.com>
2023-07-06 16:21:05 -0300

    my angular to do project

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Page
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItemsComponent,
+    TodoListComponent,
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class HomeModule { }
